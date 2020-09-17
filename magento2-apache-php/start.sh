a66d4f12afb48cfba1ca29174d2d376fc707d815
rsurfings <rsurfings@gmail.com>
2020-09-17 12:05:49 -0700

    first commit

---
+++ magento2-apache-php/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+trap custom_terminate HUP INT QUIT KILL EXIT TERM
+function custom_terminate() {
+  killall apache2
+  exit 0
+}
+rm -f /var/run/apache2/apache2.pid
+
+service apache2 start
+
+while true; do sleep 1; done
---
+++ magento2/app/code/Covid19/HelloWorld/Model/Post/Collection.php
@@ -0,0 +1,20 @@
+<?php
+namespace Covid19\HelloWorld\Model\ResourceModel\Post;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+	protected $_idFieldName = 'post_id';
+	protected $_eventPrefix = 'covid19_helloworld_post_collection';
+	protected $_eventObject = 'post_collection';
+
+	/**
+	 * Define resource model
+	 *
+	 * @return void
+	 */
+	protected function _construct()
+	{
+		$this->_init('Covid19\HelloWorld\Model\Post', 'Covid19\HelloWorld\Model\ResourceModel\Post');
+	}
+
+}
