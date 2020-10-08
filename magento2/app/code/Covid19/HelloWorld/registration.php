58791c43dea25ed843eb054d01bf0fb7a95fac15
rsurfings <rsurfings@gmail.com>
2020-10-08 06:07:33 -0700

    bug fix

--- magento2/app/code/Covid19/HelloWorld/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © 2016 Magento. All rights reserved. * See COPYING.txt for license details.
- */
-\Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Covid19_HelloWorld',
-	__DIR__
-);
